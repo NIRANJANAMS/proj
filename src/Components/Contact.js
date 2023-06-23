6cb6f838a5178a1b88424eda011c41915f979102
Amrutha P <amruthap995@gmail.com>
2023-06-23 14:32:19 +0530

    initial

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { TextField,Button } from '@mui/material'
 import './Contact.css'
 function Contact() {
   return (
@@ -6,6 +7,12 @@ function Contact() {
   <div className='contacthead'>
     <h1>contact</h1>
   </div>
+  <br />
+        <TextField variant='outlined' label='Name' /><br /><br />
+        <TextField variant='outlined' label='Email' /><br /><br /> 
+        <TextField variant='outlined' label='Number' /><br /><br /> 
+        <TextField variant='outlined' label='Message' /><br /><br />  
+        <Button variant='contained' color='secondary'>Contact</Button>
     </div>
   )
 }
